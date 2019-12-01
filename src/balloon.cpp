f937a3c6ea33d6480717f1970f832d123882d485
Julian Valentin <julian.valentin@bsplines.org>
2019-12-01 14:35:06 +0100

    Update copyright banner

--- src/balloon.cpp
+++ src/balloon.cpp
@@ -1,21 +1,7 @@
 /*
- *      Santa Racer
- *      balloon.cpp
- *
- *      Copyright (C) 2010 Julian Valentin.
- *
- *      This program is free software: you can redistribute it and/or modify
- *      it under the terms of the GNU General Public License as published by
- *      the Free Software Foundation, either version 3 of the License, or
- *      (at your option) any later version.
- *
- *      This program is distributed in the hope that it will be useful,
- *      but WITHOUT ANY WARRANTY; without even the implied warranty of
- *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *      GNU General Public License for more details.
- *
- *      You should have received a copy of the GNU General Public License
- *      along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Santa Racer - Copyright (C) 2010--2019 Julian Valentin.
+ * This code is licensed under the GNU General Public License (GNU GPL), version 3.
+ * See LICENSE.md in the project's root directory.
  */
 
 #include "balloon.hpp"
